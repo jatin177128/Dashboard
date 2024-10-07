70884382bebcbde0c8c13fb1840bb29d278855ef
jatin177128 <aryajatin177@gmail.com>
2024-10-07 16:49:23 +0530

    bg-effect

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-background via-background to-background font-sans antialiased">
-      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] " />
+     <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]" />
       <Navbar onMobileMenuClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} />
       <div className="relative flex h-[calc(100vh-3.5rem)]">
         <Sidebar 
